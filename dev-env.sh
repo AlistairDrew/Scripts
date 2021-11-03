a7f65c5251c70d50c92550baad0e57ec7183e142
Alistair Drew <alistairdrew37@gmail.com>
2021-11-03 13:34:24 +0000

    update

--- dev-env.sh
+++ dev-env.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
 
-/snap/bin/code
-/usr/bin/github-desktop
-/usr/bin/gnome-todo
-/snap/bin/insomnia
-/snap/bin/cacher
-
+code & github-desktop & gnome-todo 
--- test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-code & github-desktop & gnome-todo 
